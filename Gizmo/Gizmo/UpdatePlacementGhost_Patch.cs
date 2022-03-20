ad81325081f7bc6774b351a2b8066d834a78fbb6
Patrick Fitzgerald <patrick.fitzgerald347@gmail.com>
2022-03-19 20:23:31 -0400

    The first OpCode doesn't appear in the current version of assembly_Valheim.dll > Player > UpdatePlacementGhost, but the rest is as it was before.

--- Gizmo/Gizmo/UpdatePlacementGhost_Patch.cs
+++ Gizmo/Gizmo/UpdatePlacementGhost_Patch.cs
@@ -11,29 +11,28 @@ namespace Gizmo
     [HarmonyPatch(typeof(Player), "UpdatePlacementGhost")]
     public static class UpdatePlacementGhost_Patch
     {
+
         static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
         {
+
             var placementAnglePatched = false;
             var codes = new List<CodeInstruction>(instructions);
             for (var i = 0; i < codes.Count; i++)
             {
                 if(!placementAnglePatched)
-                if (codes[i].opcode == OpCodes.Callvirt &&
+                if (codes[i + 0].opcode == OpCodes.Ldc_R4 &&
                     codes[i + 1].opcode == OpCodes.Ldc_R4 &&
-                    codes[i + 2].opcode == OpCodes.Ldc_R4 &&
-                    codes[i + 3].opcode == OpCodes.Ldarg_0 &&
-                    codes[i + 4].opcode == OpCodes.Ldfld &&
-                    codes[i + 5].opcode == OpCodes.Conv_R4 &&
-                    codes[i + 6].opcode == OpCodes.Mul &&
-                    codes[i + 7].opcode == OpCodes.Ldc_R4 &&
-                    codes[i + 8].opcode == OpCodes.Call
+                    codes[i + 2].opcode == OpCodes.Ldarg_0 &&
+                    codes[i + 3].opcode == OpCodes.Ldfld &&
+                    codes[i + 4].opcode == OpCodes.Conv_R4 &&
+                    codes[i + 5].opcode == OpCodes.Mul &&
+                    codes[i + 6].opcode == OpCodes.Ldc_R4 &&
+                    codes[i + 7].opcode == OpCodes.Call
                     )
-
                 {
-                    codes[i + 8] = CodeInstruction.Call(typeof(Plugin), "GetPlacementAngle");
-                        placementAnglePatched = true;
+                    codes[i + 7] = CodeInstruction.Call(typeof(Plugin), "GetPlacementAngle");
+                    placementAnglePatched = true;
                 }
-
 
             }
             return codes.AsEnumerable();
